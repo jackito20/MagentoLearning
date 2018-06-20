5978b2963e5f1146fb56394f889eee91a7b1aac7
jackito <jackito_22062004@hotmail.com>
2018-06-20 11:39:50 -0500

    .

--- app/code/local/Crud/Pro/Block/Adminhtml/Pro.php
+++ app/code/local/Crud/Pro/Block/Adminhtml/Pro.php
@@ -1,12 +1,11 @@
 <?php
- class Crud_Pro_Block_Adminhtml_Pro extends Mage_Adminhtml_Block_Widget_Grid_Container
- {
- public function __construct()
- {
- $this->_controller = 'adminhtml_pro';
- $this->_blockGroup = 'pro';
- $this->_headerText = Mage::helper('pro')->__('View Data');
- $this->_addButtonLabel = Mage::helper('pro')->__('Edit Status');
- parent::__construct();
- }
+ class Crud_Pro_Block_Adminhtml_Pro extends Mage_Adminhtml_Block_Widget_Grid_Container{
+    
+    public function __construct() {
+        $this->_controller = 'adminhtml_pro';
+        $this->_blockGroup = 'pro';
+        $this->_headerText = Mage::helper('pro')->__('View Data');
+        $this->_addButtonLabel = Mage::helper('pro')->__('Edit Status');
+        parent::__construct();
+    }
  }
--- app/code/local/Crud/Pro/Block/Adminhtml/Pro/Grid.php
+++ app/code/local/Crud/Pro/Block/Adminhtml/Pro/Grid.php
@@ -1,74 +1,74 @@
 <?php
 
-class Crud_Pro_Block_Adminhtml_Pro_Grid extends Mage_Adminhtml_Block_Widget_Grid
- {
- public function __construct()
- {
- parent::__construct();
- $this->setId('proGrid');
- $this->setDefaultSort('std_id'); // This is the primary key of the database
- $this->setDefaultDir('ASC');
- $this->setSaveParametersInSession(true);
- $this->setUseAjax(true);
- }
+class Crud_Pro_Block_Adminhtml_Pro_Grid extends Mage_Adminhtml_Block_Widget_Grid {
 
-protected function _prepareCollection()
- {
- $collection = Mage::getModel('pro/pro')->getCollection();
- $this->setCollection($collection);
- return parent::_prepareCollection();
- }
+    public function __construct()
+    {
+        parent::__construct();
+        $this->setId('proGrid');
+        $this->setDefaultSort('std_id'); // This is the primary key of the database
+        $this->setDefaultDir('ASC');
+        $this->setSaveParametersInSession(true);
+        $this->setUseAjax(true);
+    }
 
-protected function _prepareColumns()
- {
- $this->addColumn('std_id', array(
- 'header' => Mage::helper('pro')->__('ID'),
- 'align' =>'right',
- 'width' => '50px',
- 'index' => 'std_id',
- ));
+    protected function _prepareCollection()
+    {
+        $collection = Mage::getModel('pro/pro')->getCollection();
+        $this->setCollection($collection);
+        return parent::_prepareCollection();
+    }
 
-$this->addColumn('stdname', array(
- 'header' => Mage::helper('pro')->__('Student Name'),
- 'align' =>'left',
- 'index' => 'stdname',
- ));
+    protected function _prepareColumns()
+    {
+        $this->addColumn('std_id', array(
+        'header' => Mage::helper('pro')->__('ID'),
+        'align' =>'right',
+        'width' => '50px',
+        'index' => 'std_id',
+        ));
 
-$this->addColumn('email', array(
- 'header' => Mage::helper('pro')->__('Email Id'),
- 'align' =>'left',
- 'index' => 'email',
- ));
+        $this->addColumn('stdname', array(
+        'header' => Mage::helper('pro')->__('Student Name'),
+        'align' =>'left',
+        'index' => 'stdname',
+        ));
 
-$this->addColumn('rollno', array(
- 'header' => Mage::helper('pro')->__('Roll_NO'),
- 'align' =>'left',
- 'index' => 'rollno',
- ));
+        $this->addColumn('email', array(
+        'header' => Mage::helper('pro')->__('Email Id'),
+        'align' =>'left',
+        'index' => 'email',
+        ));
 
-$this->addColumn('status', array(
+        $this->addColumn('rollno', array(
+        'header' => Mage::helper('pro')->__('Roll_NO'),
+        'align' =>'left',
+        'index' => 'rollno',
+        ));
 
-'header' => Mage::helper('pro')->__('Status'),
- 'align' => 'left',
- 'width' => '80px',
- 'index' => 'status',
- 'type' => 'options',
- 'options' => array(
- 1 => 'Active',
- 0 => 'Inactive',
- ),
- ));
+        $this->addColumn('status', array(
 
-return parent::_prepareColumns();
- }
+        'header' => Mage::helper('pro')->__('Status'),
+        'align' => 'left',
+        'width' => '80px',
+        'index' => 'status',
+        'type' => 'options',
+        'options' => array(
+        1 => 'Active',
+        0 => 'Inactive',
+        ),
+        ));
 
-public function getRowUrl($row)
- {
- return $this->getUrl('*/*/edit', array('id' => $row->getId()));
- }
+        return parent::_prepareColumns();
+    }
 
-public function getGridUrl()
- {
- return $this->getUrl('*/*/grid', array('_current'=>true));
- }
- }
+    public function getRowUrl($row)
+    {
+        return $this->getUrl('*/*/edit', array('id' => $row->getId()));
+    }
+
+    public function getGridUrl()
+    {
+        return $this->getUrl('*/*/grid', array('_current'=>true));
+    }
+}
